b47328f1e69990be1632f5b6ff1674aa92efbe88
bockariejuniomansaray <bmansaray1@byupathway.edu>
2025-03-07 01:39:15 -0800

    my first update.

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+// Insert current year in the footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Insert last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
