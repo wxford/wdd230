ab41ec0790a7ec9398ce293c7edf56c0e0ed0181
bockariejuniomansaray <bmansaray1@byupathway.edu>
2025-03-21 07:16:07 -0700

    week 3

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,13 @@ document.getElementById("year").textContent = new Date().getFullYear();
 
 // Insert last modified date
 document.getElementById("lastModified").textContent = document.lastModified;
+// localStorage for Page Visits Counter
+if (localStorage.getItem('visitCount')) {
+    let count = parseInt(localStorage.getItem('visitCount'));
+    count += 1;
+    localStorage.setItem('visitCount', count);
+    document.getElementById('visit-count').textContent = count;
+} else {
+    localStorage.setItem('visitCount', 1);
+    document.getElementById('visit-count').textContent = 1;
+}
